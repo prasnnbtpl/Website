6cc1accbcdce41012f3d91df528cd8999aadb34b
prasannbtpl <prasann.btpl@gmail.com>
2025-06-23 12:44:52 +0530

    -F

--- Ecommerce/src/App.tsx
+++ Ecommerce/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,17 @@ function App() {
   return (
     <div>
       <header>BTPL</header>
-      <main></main>
+      <main>
+        <ul>
+        {
+        sampleProducts.map(products => 
+        <li key={products.slug}>
+          <img src={products.image} alt={products.name} className='product-image' />
+          <h2>{products.name}</h2>
+          <p>{products.price}</p>
+        </li>)}   
+        </ul>
+      </main>
       <footer>All Right Reserved</footer>
     </div>
   )
--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 2. Install Tools
 3. Create Typescript react App By vite
 4. Create Git Repository
+    1. add README.md
+    2. Create github Account
+    3. Connect vs code to github
+    4. publish repository.
+    
+    
