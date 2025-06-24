20cb76dd28fa1ff83a7960eeb4d341a2853f52ee
prasannbtpl <prasann.btpl@gmail.com>
2025-06-24 11:47:05 +0530

    Link run

---
+++ Backend/src/data.ts
@@ -0,0 +1,52 @@
+import type { Products } from "./types/Products";
+
+export const sampleProducts:Products[]=[
+    {
+        name: 'Nike slim shirt',
+        slug: "nike-slim-shirt",
+        image: "../images/shirt1.jpg",
+        category: "Shirt",
+        brand: "Nike",
+        price: "150 Rs",
+        countInStock: "60",
+        description: "high quality shirt",
+        rating: "4.5",
+        numReviews: "30"
+    },
+    {
+        name: 'Adidas Fit shirt',
+        slug: "adidas-fir-shirt",
+        image: "../images/shirt3.jpg",
+        category: "Shirt",
+        brand: "Adidas",
+        price: "199 Rs",
+        countInStock: "45",
+        description: "high quality shirt",
+        rating: "4.9",
+        numReviews: "28"
+    },
+    {
+        name: 'Lacoste Free Pants',
+        slug: "lacoste-free-pants",
+        image: "../images/pants1.jpg",
+        category: "Pants",
+        brand: "lacoste",
+        price: "199 Rs",
+        countInStock: "33",
+        description: "high quality product",
+        rating: "4.3",
+        numReviews: "24"
+    },
+    {
+        name: 'Nike slim Pants',
+        slug: "nike-slim-pants",
+        image: "../images/pants3.jpg",
+        category: "Pants",
+        brand: "Nike",
+        price: "249 Rs",
+        countInStock: "20",
+        description: "high quality product",
+        rating: "4.7",
+        numReviews: "38"
+    }
+]
---
+++ Backend/src/types/Products.ts
@@ -0,0 +1,13 @@
+export type Products ={
+    name:string
+    slug:string
+    image:string
+    category:string
+    brand:string
+    price:string
+    countInStock:string
+    description:string
+    rating:string
+    numReviews:string
+    
+}
